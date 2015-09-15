f8314cdf244a7d7375c95b3c12f2193a4a42df5a
Rub21 <ruben@mapbox.com>
2015-09-15 13:19:29 -0500

    first version count

--- historical.sh
+++ historical.sh
@@ -4,11 +4,29 @@ while read -r line; do
     echo decompres ${line##*/}
     echo bzip2 -d ${line##*/}
     base=${line##*/}
-    echo osmconvert ${base%.*} -B=boundary/gb.poly -o=gb-${base%.*}
-    osmfilter gb-${base%.*} --keep="highway=" -o=highway-gb-${base%.*}
-    osmfilter gb-${base%.*} --keep="building=" -o=building-highway-gb-${base%.*}
-    osmfilter gb-${base%.*} --keep="leisure= amenity=" -o=pois-highway-gb-${base%.*}
+    osmconvert ${base%.*} -B=boundary/gb.poly -o=gb-${base%.*}
+    #osmfilter gb-${base%.*} --keep="highway=" -o=highway-gb-${base%.*}
+    #osmfilter gb-${base%.*} --keep="building=" -o=building-highway-gb-${base%.*}
+    #osmfilter gb-${base%.*} --keep="leisure= amenity=" -o=pois-highway-gb-${base%.*}
     # merger
-    osmconvert highway-gb-${base%.*} building-highway-gb-${base%.*} pois-highway-gb-${base%.*} -o=result-gb-${base%.*}
-    
+    #osmconvert highway-gb-${base%.*} building-highway-gb-${base%.*} pois-highway-gb-${base%.*} -o=result-gb-${base%.*}
 done < historical-list
+
+    # esto se implementa solo para filtrar las fechas:
+    echo  Num objects 2010
+    #1262304000 = 01/01/2010
+    node index.js node index.js --osmfile=gb-planet-101229.osm --timestamp=1262304000
+
+    echo  Num objects 2011
+    #1293840000 = 01/01/2012
+    node index.js node index.js --osmfile=gb-planet-111228.osm --timestamp=1293840000
+    echo  Num objects 2012
+    #1325376000 = 01/01/2012
+    node index.js node index.js --osmfile=gb-planet-121226.osm --timestamp=1325376000
+    echo  Num objects 2013
+    #1356998400 = 01/01/2013
+    node index.js node index.js --osmfile=gb-planet-131225.osm --timestamp=1356998400
+    echo  Num objects 2014
+    #1388534400 = 01/01/2014
+    node index.js node index.js --osmfile=gb-planet-111228.osm --timestamp=1388534400
+
