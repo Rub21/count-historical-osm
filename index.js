b8a81f6ace1f624efb9b51430bde3160d641e2fb
Rub21 <ruben@mapbox.com>
2015-09-15 17:48:10 -0500

    add shop to counter

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var reader = new osmium.Reader(osmfile);
 var handler = new osmium.Handler();
 
 handler.on('node', function(node) {
-        if (node.timestamp_seconds_since_epoch >= timestamp && (node.tags().amenity !== undefined || node.tags().leisure !== undefined)) {
+        if (node.timestamp_seconds_since_epoch >= timestamp && (node.tags().amenity !== undefined || node.tags().leisure !== undefined || node.tags().shop !== undefined)) {
                 if (node.version === 1) {
                         counter.osm_node_v1++;
                 } else {
@@ -33,7 +33,7 @@ handler.on('node', function(node) {
 });
 
 handler.on('way', function(way) {
-        if (way.timestamp_seconds_since_epoch >= timestamp && (way.tags().highway != undefined || way.tags().building != undefined)) {
+        if (way.timestamp_seconds_since_epoch >= timestamp && (way.tags().highway !== undefined || way.tags().building !== undefined)) {
                 if (way.version === 1) {
                         counter.osm_way1++;
                 } else {
@@ -42,7 +42,7 @@ handler.on('way', function(way) {
         }
 });
 handler.on('relation', function(relation) {
-        if (relation.timestamp_seconds_since_epoch >= timestamp && (relation.tags().highway != undefined || relation.tags().building != undefined)) {
+        if (relation.timestamp_seconds_since_epoch >= timestamp && (relation.tags().highway !== undefined || relation.tags().building !== undefined)) {
                 if (relation.version === 1) {
                         counter.osm_relation1++;
                 } else {
