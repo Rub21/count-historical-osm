1b3177c18276fe9421d389dba278714c91a9b4c1
Rub21 <ruben@mapbox.com>
2015-09-16 17:06:14 -0500

    fix to proces date diary

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ var osmium = require('osmium');
 var argv = require('optimist').argv;
 var _ = require('underscore');
 var osmfile = argv.osmfile;
+var conString = "postgres://postgres:1234@localhost/dbosm";
+var client = new pg.Client(conString);
+client.connect(function(err) {
+        if (err) {
+                return console.error('could not connect to postgres', err);
+        }
+});
 
 var obj = function() {
         return {
-                timestamp: 0,
-                numfile: osmfile.split('.')[0],
+                osm_timestamp: 0,
+                numfile: parseInt(osmfile.split('.')[0]),
                 allnodes: 0, //all nodes
                 allways: 0, //all ways
-                allrealtions: 0, //all relations
+                allrelations: 0, //all relations
                 nodev1: 0, //amenity,leisure and shop
                 nodevx: 0, //amenity,leisure and shop
                 way1: 0, //highway and buildings
@@ -27,7 +34,7 @@ var reader = new osmium.Reader(osmfile);
 var handler = new osmium.Handler();
 
 handler.on('node', function(node) {
-        counter.timestamp = node.timestamp_seconds_since_epoch - node.timestamp_seconds_since_epoch % 1000;
+        counter.osm_timestamp = node.timestamp_seconds_since_epoch - node.timestamp_seconds_since_epoch % 1000;
 
         counter.allnodes++;
         if (node.tags().amenity !== undefined || node.tags().leisure !== undefined || node.tags().shop !== undefined) {
@@ -61,8 +68,11 @@ handler.on('relation', function(relation) {
 });
 
 osmium.apply(reader, handler);
-var outputFilename = osmfile.split('.')[0] + '.json';
-fs.writeFile(outputFilename, JSON.stringify(counter), function(err) {
+
+var query = 'INSERT INTO osm2104(osm_timestamp,numfile,allnodes,allways,allrelations,nodev1,nodevx,way1,wayx,relation1,relationx)  VALUES ($1, $2, $3,$4,$5,$6,$7,$8,$9,$10,$11);';
+var data = [counter.osm_timestamp, counter.numfile, counter.allnodes, counter.allways, counter.allrelations, counter.nodev1, counter.nodevx, counter.way1, counter.wayx, counter.relation1, counter.relationx]
+
+client.query(query, data, function(err, result) {
         if (err) {
                 console.log(err);
         }
---
+++ query/tables.sql
@@ -0,0 +1,73 @@
+CREATE TABLE osm2014(
+	osm_timestamp integer,
+	numfile integer,
+	allnodes integer,
+	allways integer,
+	allrelations integer,
+	nodev1 integer,
+	nodevx integer,
+	way1 integer,
+	wayx integer,
+	relation1 integer,
+	relationx integer
+);
+
+
+CREATE TABLE osm2013(
+	osm_timestamp integer,
+	numfile integer,
+	allnodes integer,
+	allways integer,
+	allrelations integer,
+	nodev1 integer,
+	nodevx integer,
+	way1 integer,
+	wayx integer,
+	relation1 integer,
+	relationx integer
+);
+
+
+CREATE TABLE osm2012(
+	osm_timestamp integer,
+	numfile integer,
+	allnodes integer,
+	allways integer,
+	allrelations integer,
+	nodev1 integer,
+	nodevx integer,
+	way1 integer,
+	wayx integer,
+	relation1 integer,
+	relationx integer
+);
+
+
+CREATE TABLE osm2011(
+	osm_timestamp integer,
+	numfile integer,
+	allnodes integer,
+	allways integer,
+	allrelations integer,
+	nodev1 integer,
+	nodevx integer,
+	way1 integer,
+	wayx integer,
+	relation1 integer,
+	relationx integer
+);
+
+
+CREATE TABLE osm2010(
+	osm_timestamp integer,
+	numfile integer,
+	allnodes integer,
+	allways integer,
+	allrelations integer,
+	nodev1 integer,
+	nodevx integer,
+	way1 integer,
+	wayx integer,
+	relation1 integer,
+	relationx integer
+);
