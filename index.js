c9e6ac3be8065a29e6b16c53c77108457998685c
Rub21 <ruben@mapbox.com>
2015-09-16 17:45:06 -0500

    fix to proces date diary

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ handler.on('relation', function(relation) {
 });
 
 osmium.apply(reader, handler);
-
 var query = 'INSERT INTO osm2014(osm_timestamp,numfile,allnodes,allways,allrelations,nodev1,nodevx,way1,wayx,relation1,relationx)  VALUES ($1, $2, $3,$4,$5,$6,$7,$8,$9,$10,$11);';
 var data = [counter.osm_timestamp, counter.numfile, counter.allnodes, counter.allways, counter.allrelations, counter.nodev1, counter.nodevx, counter.way1, counter.wayx, counter.relation1, counter.relationx]
 client.query(query, data, function(err, result) {
