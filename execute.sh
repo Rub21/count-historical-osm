418c02da591fe5f16a87f47877f6185687955145
Rub21 <ruben@mapbox.com>
2015-09-18 15:06:17 -0500

    db

--- execute.sh
+++ execute.sh
@@ -11,6 +11,8 @@ touch log2014
 chmod 777 log2014
 cp ../osm2pg.sh .
 cp ../index.js .
+mkdir boundary
+cp ../boundary/gb.poly boundary/
 nohup ./osm2pg.sh 476 840 2014 > log2014  2>&1 &
 
 cd ..
@@ -21,6 +23,8 @@ touch log2013
 chmod 777 log2013
 cp ../osm2pg.sh .
 cp ../index.js .
+mkdir boundary
+cp ../boundary/gb.poly boundary/
 nohup ./osm2pg.sh 11 475 2013 > log2013  2>&1 &
 
 
